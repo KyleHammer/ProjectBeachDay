18b36196fc325490ced224b1dc4622ed4adebfbf
Kyle [Home PC] <kylethomashammer@gmail.com>
2022-12-04 12:13:26 +1100

    save before removing lines of code

--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -23,22 +23,48 @@ public class Gun : MonoBehaviour
 
     private void Update()
     {
-        Vector2 weaponPosition = transform.position;
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        Vector2 direction = mousePosition - weaponPosition;
-        transform.right = direction;
-        
-        if (transform.localRotation.z > 0.7 || transform.localRotation.z < -0.7)
-            sr.flipY = true;
-        else
-            sr.flipY = false;
+        SetGunDirection();
+        SetSpriteDirection();
     }
 
     public void Shoot()
     {
         GameObject newProjectile = Instantiate(projectile, shotPoint.position, shotPoint.rotation);
-        newProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * projectileSpeed;
-        newProjectile.GetComponent<Projectile>().SetDamage(currentStats.damage);
+        Debug.Log("Bang");
+        
+        // TODO: Implement bullet movement
+        
+        // Set the projectile velocity
+        // newProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * projectileSpeed;
+        
+        // Set the projectile damage
+        // newProjectile.GetComponent<Projectile>().SetDamage(currentStats.damage);
+    }
+
+    private void SetGunDirection()
+    {
+        // TODO: Implement gun rotation
+        
+        // Find the mouse position on the screen
+        //Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        
+        // Get the position of the gun
+        //Vector2 weaponPosition = transform.position;
+        
+        // Get the direction the bullet needs to go (end point - start point)
+        //Vector2 direction = mousePosition - weaponPosition;
+        
+        // Set the gun transform's right side to the direction
+        // This is because the gun faces right by default
+        // transform.right = direction;
+    }
+
+    private void SetSpriteDirection()
+    {
+        if (transform.localRotation.z > 0.7 || transform.localRotation.z < -0.7)
+            sr.flipY = true;
+        else
+            sr.flipY = false;
     }
 
     public void IncreaseDamage(float increase)
